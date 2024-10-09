0e0999d925d02b30db619d962546178b5926e108
LuciaHerreroAlvarez <herreroalvarezlucia@gmail.com>
2024-10-09 18:08:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,15 +3,6 @@
 // ///////////////// Inicialización básica///////////////////////////////////////////////////
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Función para manejar el cambio de idioma
-  function setLanguage(language) {
-    if (language === 'es') {
-      window.location.href = 'index.html'; // Redirigir a la versión en español
-    } else if (language === 'en') {
-      window.location.href = 'index_en.html'; // Redirigir a la versión en inglés
-    }
-  }
-
   // Seleccionar botones de las pestañas y el contenido de las pestañas
     const tabButtons = document.querySelectorAll('.tab-button');
     const tabContents = document.querySelectorAll('.tab-content');
